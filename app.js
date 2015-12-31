5bf1e5973cf7b86213a6a9784699ddeb8e656114
chrischou <chrischou5050@sina.cn>
2015-12-31 14:03:04 +0800

    add files

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
 var path = require('path'); 
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parse');
+var session = require('express-session');
+var Controllers = require('./controllers');
 
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended:true
+}));
+app.use(cookieParser())
+app.use(session({
+	secret:'chatting',
+	resave:true,
+	saveUninitialized:false
+	cookie:{
+		maxAge:60*1000
+	}
+}))
 
 app.use(express.static(path.join(__dirname,'static')));
 app.use(function(req,res){
@@ -29,3 +47,42 @@ io.sockets.on('connection',function(socket){
 });
 
 
+app.get('/api/validate',function(req,res){
+	var _userId = req.session._userId;
+	if (_userId) {
+		Controllers.User.findUserById(_userId,function(err,user){
+			if(err){
+				res.json(401,{
+					msg:err
+				})
+			} else {
+				res.json(user)
+			}
+		})
+	} else {
+		res.json(401,null)
+	}
+})
+
+app.post('/api/login',function(req,res){
+	var email = req.body.email;
+	if(email){
+		Controllers.User.findByEmailOrCreate(email,function(err,user){
+			if(err){
+				res.json(500,{
+					msg:err
+				})
+			} else {
+				req.session._userId = user._id
+				res.json(user)
+			}
+		})
+	} else {
+		res.json(403)
+	}
+})
+
+app.get('/api/logout',function(req,res){
+	req.session._userId = null
+	res.json(401)
+})
---
+++ model/index.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/chatting');
+exports.User = mongoose.model('User',require('./user'));
+
+exports.User = require('./user');
