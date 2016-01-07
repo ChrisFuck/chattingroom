8b81998a4ca6c6ab1de555b38f445b12f117d68c
chrischou <chrischou5050@sina.cn>
2016-01-07 17:37:54 +0800

    修改试图路径

--- static/router.js
+++ static/router.js
@@ -1,10 +1,10 @@
 angular.modul('technodeApp').config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
 	$locationProvider.html5Mode(true)
 	$routeProvider.when('/',{
-		templateUrl:'pages/room.html',
+		templateUrl:'/pages/room.html',
 		controller:'RoomCtrl'
 	}).when('/login',{
-		templateUrl:'pages/login.html',
+		templateUrl:'/pages/login.html',
 		controller:'LoginCtrl'
 	}).otherwise({
 		redirectTo:'/login'
